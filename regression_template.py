95cff54dd113b1fd0f56082acdb8bae394c9e597
Adrian Maulana Muhammad <adriantoto7@gmail.com>
2019-01-10 15:19:59 +0700

    Create regression_template.py

---
+++ regression_template.py
@@ -0,0 +1,51 @@
+# Regression Template
+
+# Regression
+
+# Importing Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing Dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+#X = pd.DataFrame(Xnd)
+y = dataset.iloc[:, 2].values
+#y = pd.DataFrame(ynd)
+
+# Splitting the dataset into the Training set and Test set
+"""from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, 
+                                                    random_state = 0)"""
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)"""
+
+# Fitting the Regression Model to the dataset
+
+
+# Predicting a new result with Regression
+y_pred = regressor.predict([[6.5]])
+
+# Visualising the Regression Results 
+plt.scatter(X,y, color = 'red')
+plt.plot(X, regressor.predict(X), color = 'blue')
+plt.title('Truth or Bluff (Regression model)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show
+
+# Visualising the Regression Results (higher resolution)
+X_grid = np.arange(min(X), max(X),0.01)
+X_grid = X_grid.reshape(len(X_grid), 1)
+plt.scatter(X,y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (Regression model)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show
+
